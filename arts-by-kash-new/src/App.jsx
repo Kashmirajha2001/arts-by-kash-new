ae92948dc26e6dfcc2eaa35a8ff5d4e9ee0be12d
cathedralcockroach <160588973+cathedralcockroach@users.noreply.github.com>
2026-06-27 00:46:58 +0530

    Initial project setup

---
+++ arts-by-kash-new/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <Button variant="contained">
+        Hello Arts by Kash
+      </Button>
+    </div>
+  );
+}
---
+++ arts-by-kash-new/src/src/Test.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+function Test() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <Button variant="contained">
+        Test Button
+      </Button>
+    </div>
+  );
+}
+
+export default Test;
