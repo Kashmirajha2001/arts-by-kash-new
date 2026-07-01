b62eef6e6467a48098573618cd3d4b310dea4eab
Kashmira Jha <kashmirajha2001@gmail.com>
2026-07-02 01:45:38 +0530

    Reset Password Error 6.0

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -14,6 +14,7 @@ const sendEmail = async ({ to, subject, html }) => {
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4, // 👈 Force IPv4
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
